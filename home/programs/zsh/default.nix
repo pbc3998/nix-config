359e2fcc8a068edc4137fb1c1faf2d82c6a8829a
PBCDev <baochaupham4096@gmail.com>
2026-07-06 13:08:33 +0700

    setup config for zsh

---
+++ home/programs/zsh/default.nix
@@ -0,0 +1,15 @@
+{
+  programs.zsh = {
+    enable = true;
+
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    history = {
+      size = 10000;
+      save = 10000;
+      share = true;
+    };
+  };
+}
