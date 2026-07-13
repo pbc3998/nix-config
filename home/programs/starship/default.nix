fe19d015c6a620c2611cb5f6b4f4759617ec550f
PBCDev <baochaupham4096@gmail.com>
2026-07-13 22:56:14 +0700

    starship: setup prompt

--- home/programs/starship/default.nix
+++ home/programs/starship/default.nix
@@ -1,5 +1,14 @@
 {
   programs.starship = {
     enable = true;
+    settings = {
+      add_newline = true;
+      format = "$directory$git_branch$git_status$dotnet$line_break$character";
+
+      character = {
+        success_symbol = "󱄅   ";
+        error_symbol = "[](bold #ff5555)   ";
+      };
+    };
   };
 }
