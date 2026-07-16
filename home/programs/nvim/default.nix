3cd7dab8c5ca9ebb114f2509edf913c5c0252649
PBCDev <baochaupham4096@gmail.com>
2026-07-16 19:06:53 +0700

    nvim: setup config for basic option

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -3,5 +3,23 @@
   programs.nixvim = {
     enable = true;
     nixpkgs.source = inputs.nixpkgs;
+
+    opts = {
+      number = true;
+      relativenumber = true;
+      signcolumn = "yes";
+      cursorline = true;
+
+      shiftwidth = 2;
+      tabstop = 2;
+      expandtab = true;
+      smartindent = true;
+
+      ignorecase = true;
+      smartcase = true;
+
+      clipboard = "unnamedplus";
+      undofile = true;
+    };
   };
 }
