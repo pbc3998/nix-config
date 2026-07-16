07fd449c88d0f625c6e7a8e74d9853590b8e64f9
PBCDev <baochaupham4096@gmail.com>
2026-07-17 00:49:31 +0700

    kitty(keymaps): remap key

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -66,22 +66,22 @@
       color15 = "#e0d9c7";
     };
     keybindings = {
-      "alt+a" = "new_tab";
-      "alt+s" = "close_tab";
+      "alt+shift+a" = "new_tab";
+      "alt+shift+s" = "close_tab";
 
-      "alt+f" = "previous_tab";
-      "alt+d" = "next_tab";
+      "alt+shift+f" = "previous_tab";
+      "alt+shift+d" = "next_tab";
 
-      "alt+1" = "goto_tab 1";
-      "alt+2" = "goto_tab 2";
-      "alt+3" = "goto_tab 3";
-      "alt+4" = "goto_tab 4";
-      "alt+5" = "goto_tab 5";
-      "alt+6" = "goto_tab 6";
-      "alt+7" = "goto_tab 7";
-      "alt+8" = "goto_tab 8";
-      "alt+9" = "goto_tab 9";
-      "alt+10" = "goto_tab 10";
+      "alt+shift+1" = "goto_tab 1";
+      "alt+shift+2" = "goto_tab 2";
+      "alt+shift+3" = "goto_tab 3";
+      "alt+shift+4" = "goto_tab 4";
+      "alt+shift+5" = "goto_tab 5";
+      "alt+shift+6" = "goto_tab 6";
+      "alt+shift+7" = "goto_tab 7";
+      "alt+shift+8" = "goto_tab 8";
+      "alt+shift+9" = "goto_tab 9";
+      "alt+shift+10" = "goto_tab 10";
     };
   };
 }
