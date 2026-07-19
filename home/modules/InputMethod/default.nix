8b686b1d7ef8a397080b08c93e53fb38ddae4588
PBCDev <baochaupham4096@gmail.com>
2026-07-19 13:23:24 +0700

    fcitx5: reconfig

--- home/modules/InputMethod/default.nix
+++ home/modules/InputMethod/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   i18n.inputMethod = {
     enable = true;
@@ -10,5 +10,9 @@
     ];
     fcitx5.waylandFrontend = true;
   };
-  xdg.configFile."fcitx5/config".source = ./fcitx5.conf;
+
+  xdg.configFile."fcitx5/config".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5.conf;
+  xdg.configFile."fcitx5/conf/clipboard.conf".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5/clipboard.conf;
+  xdg.configFile."fcitx5/conf/notifications.conf".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5/notifications.conf;
+  xdg.configFile."fcitx5/profile".source = config.lib.file.mkOutOfStoreSymlink ./fcitx5/profile;
 }
