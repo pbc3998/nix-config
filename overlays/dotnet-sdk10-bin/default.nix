04ca169a5bbf4e8a2b77c3d71a3b4c643b223af3
PBCDev <baochaupham4096@gmail.com>
2026-07-23 09:35:54 +0700

    overlays(dotnet-sdk10-bin): add roslyn

--- overlays/dotnet-sdk10-bin/default.nix
+++ overlays/dotnet-sdk10-bin/default.nix
@@ -1,4 +1,9 @@
 final: prev: {
   dotnet-sdk_10 = prev.dotnetCorePackages.sdk_10_0-bin;
   dotnet-runtime_10 = prev.dotnetCorePackages.runtime_10_0-bin;
+  roslyn-ls = prev.roslyn-ls.override {
+    dotnetCorePackages = prev.dotnetCorePackages // {
+      sdk_10_0 = prev.dotnetCorePackages.sdk_10_0-bin;
+    };
+  };
 }
